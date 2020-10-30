72dd390ea7dfb567d6d13d5041ae65914f79cfe4
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2020-10-30 08:39:38 -0300

    Primeiro commit

---
+++ app.ts
@@ -0,0 +1,53 @@
+﻿import express = require("express");
+import cookieParser = require("cookie-parser");
+import passport = require("passport");
+import appsettings = require("./appsettings");
+
+const app = express();
+
+app.disable("x-powered-by");
+app.disable("etag");
+
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+	res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+	res.header("Expires", "-1");
+	res.header("Pragma", "no-cache");
+	next();
+});
+
+const OIDCStrategy = require("passport-azure-ad").OIDCStrategy;
+
+passport.use(new OIDCStrategy({
+	identityMetadata: appsettings.adAuthority + appsettings.adTenantId + appsettings.adIdMetadata,
+	clientID: appsettings.adApplicationId,
+	responseType: "code id_token",
+	responseMode: "form_post",
+	redirectUrl: appsettings.adRedirectUrl,
+	allowHttpForRedirectUrl: true,
+	clientSecret: appsettings.adClientSecret,
+	validateIssuer: false,
+	passReqToCallback: false,
+    useCookieInsteadOfSession: true,
+    cookieEncryptionKeys: appsettings.adCookieEncryptionKeys,
+	scope: appsettings.adScopes
+}, (iss: any, sub: any, profile: any, accessToken: any, refreshToken: any, params: any, done: any): any => {
+	// done(err: any, user: any, info: any), executa o callback em routes\ad.ts
+	return (profile.oid ? done(null, profile, accessToken) : done(new Error("Usuário do Microsoft AD sem OID."), null, null));
+}));
+
+app.use(appsettings.routePrefix, require("./routes/ad"));
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+	res.status(err.status || 500);
+
+	res.json(err.status == 404 ? "Não encontrado" : (err.message || err.toString()));
+});
+
+const server = app.listen(parseInt(process.env.PORT) || 3010, process.env.IP || "127.0.0.1", () => {
+	console.log("Express server listening on port " + server.address()["port"]);
+});
---
+++ routes/ad.ts
@@ -0,0 +1,143 @@
+﻿import { randomBytes } from "crypto";
+import express = require("express");
+import graph = require('@microsoft/microsoft-graph-client');
+import passport = require("passport");
+import appsettings = require("../appsettings");
+import intToHex = require("../utils/intToHex");
+
+require("isomorphic-fetch");
+
+const router = express.Router();
+
+class Usuario {
+	public user: string;
+	public nome: string;
+	public email: string;
+	public emailAcademico: string;
+	public aluno: boolean;
+}
+
+interface Resultado {
+	token: string;
+	erro: string;
+	dados: Usuario;
+}
+
+const maxResultados = (1 << 14), // 2 ^ 14 = 16384
+	maxResultadosMask = maxResultados - 1,
+	resultados: Resultado[] = new Array(maxResultados);
+let ultimoResultado = 0;
+
+function adLogin(req: express.Request, res: express.Response, next: express.NextFunction) {
+	// https://www.npmjs.com/package/passport-azure-ad
+	// https://docs.microsoft.com/en-us/graph/tutorials/node
+	passport.authenticate("azuread-openidconnect", { session: false }, async (err: any, user: any, info: any) => {
+		let erro: string = null;
+		let dados: Usuario = null;
+
+		try {
+			if (err) {
+				erro = (err.message || err.toString());
+			} else {
+				// https://www.npmjs.com/package/@microsoft/microsoft-graph-client
+				// https://github.com/microsoftgraph/msgraph-sdk-javascript
+				// https://github.com/microsoftgraph/msgraph-training-nodeexpressapp/blob/main/tutorial/04-add-aad-auth.md
+
+				// info é o accessToken, enviado pelo callback em app.ts
+				const client = graph.Client.init({ authProvider: (done) => { done(null, info); } });
+				const user = await client.api("/me").get();
+
+				dados = new Usuario();
+				dados.nome = (user.displayName as string || "").trim() || (user.givenName as string || "").trim();
+				if (!dados.nome) {
+					erro = "Informações sobre o nome do usuário faltantes no AD";
+					dados = null;
+				} else {
+					dados.user = (user.userPrincipalName as string || "").trim().toLowerCase();
+					if (!dados.user || (!dados.user.endsWith("@espm.br") && !dados.user.endsWith("@acad.espm.br"))) {
+						dados.user = (user.mail || "").trim();
+						if (!dados.user || (!dados.user.endsWith("@espm.br") && !dados.user.endsWith("@acad.espm.br"))) {
+							erro = "Informações sobre o login do usuário faltantes no AD";
+							dados = null;
+						} else {
+							dados.email = (user.userPrincipalName as string || "").trim().toLowerCase();
+						}
+					} else {
+						dados.email = (user.mail as string || "").trim().toLowerCase();
+					}
+					if (dados) {
+						dados.emailAcademico = dados.user;
+						if (!dados.email)
+							dados.email = dados.user;
+						dados.aluno = dados.user.endsWith("@acad.espm.br");
+						dados.user = dados.user.substring(0, dados.user.lastIndexOf("@"));
+						if (!dados.user) {
+							erro = "E-mail de login do usuário está inválido no AD";
+							dados = null;
+						}
+					}
+				}
+			}
+		} catch (ex) {
+			erro = ex.message || ex.toString();
+			dados = null;
+		}
+
+		const callback = req.cookies[appsettings.cookieName] as string;
+		res.cookie(appsettings.cookieName, "", { expires: new Date(0), httpOnly: true, path: "/", secure: false });
+		if (!callback) {
+			res.status(400).json("Parâmetro callback faltando!");
+		} else {
+			ultimoResultado++;
+			ultimoResultado &= maxResultadosMask;
+			const resultado: Resultado = {
+				token: intToHex(ultimoResultado ^ appsettings.hashId) + randomBytes(16).toString("hex"),
+				erro: erro,
+				dados: dados
+			};
+			resultados[ultimoResultado] = resultado;
+			res.redirect(callback + ((callback.indexOf("?") >= 0) ? "&token=" : "?token=") + resultado.token);
+		}
+	})(req, res, next);
+}
+
+// Alguns iPhones redirecionam apenas para /ad, e não para /ad/login...
+router.all("/", adLogin);
+
+router.all("/login", adLogin);
+
+router.all("/redir", (req: express.Request, res: express.Response) => {
+	const callback = req.query["callback"] as string;
+	if (!callback) {
+		res.status(400).json("Parâmetro callback faltando!");
+		return;
+	}
+	res.cookie(appsettings.cookieName, callback, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true, path: "/", secure: false });
+	res.redirect(appsettings.adRedirectUrl);
+});
+
+router.get("/token/:token", (req: express.Request, res: express.Response) => {
+	const token = req.params["token"] as string;
+	let id: number;
+	if (!token ||
+		token.length !== 40 ||
+		isNaN(id = parseInt(token.substr(0, 8), 16)) ||
+		(id = (id ^ appsettings.hashId)) < 0 ||
+		id >= maxResultados) {
+		res.json({ token: null, erro: "Token inválido", dados: null });
+		return;
+	}
+	const resultado = resultados[id];
+	if (!resultado) {
+		res.json({ token: null, erro: "Dados nulos para o token requisitado", dados: null });
+		return;
+	}
+	if (resultado.token !== token) {
+		res.json({ token: null, erro: "Token não confere", dados: null });
+		return;
+	}
+	resultados[id] = null;
+	res.json(resultado);
+});
+
+export = router;
